422f4ba1e07a3e055e71027e75f8b3bfde09b52d
BenoitMorel <morelbt@hitssv543.villa-bosch.de>
2023-10-07 13:07:15 +0200

    Add forgotten file

---
+++ src/tools/order_speciations.cpp
@@ -0,0 +1,25 @@
+#include <trees/DatedTree.hpp>
+#include <iostream>
+#include <fstream>
+
+
+int main(int argc, char** argv)
+{
+  if (argc != 3) {
+    std::cerr << "Error, syntax is:" << std::endl;
+    std::cerr << "order_speciation input_tree output_tree" << std::endl;
+    return 1;
+  }
+  std::string input(argv[1]);
+  std::string output(argv[2]);
+  PLLRootedTree rootedTree(input, true);
+  DatedTree datedTree(&rootedTree, true);
+  datedTree.rescaleBranchLengths();
+  std::ofstream os(output);
+  os << rootedTree.getNewickString() << std::endl;
+  return 0;
+}
+
+
+
+
